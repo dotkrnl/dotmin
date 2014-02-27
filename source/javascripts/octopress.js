b7c914eb7337789e9e8c08ac2d1584aed6d1d4b0
Jason Lau <i@dotkrnl.com>
2014-02-27 12:21:50 +0800

    working version
    working sidebar
    working responsive
    working comment
    working github

---
+++ source/javascripts/octopress.js
@@ -0,0 +1,75 @@
+function testFeatures() {
+  var features = ['maskImage'];
+  $(features).map(function(i, feature) {
+    if (Modernizr.testAllProps(feature)) {
+      $('html').addClass(feature);
+    } else {
+      $('html').addClass('no-'+feature);
+    }
+  });
+  if ("placeholder" in document.createElement("input")) {
+    $('html').addClass('placeholder');
+  } else {
+    $('html').addClass('no-placeholder');
+  }
+}
+
+function addCodeLineNumbers() {
+  if (navigator.appName === 'Microsoft Internet Explorer') { return; }
+  $('div.gist-highlight').each(function(code) {
+    var tableStart = '<table><tbody><tr><td class="gutter">',
+        lineNumbers = '<pre class="line-numbers">',
+        tableMiddle = '</pre></td><td class="code">',
+        tableEnd = '</td></tr></tbody></table>',
+        count = $('.line', code).length;
+    for (var i=1;i<=count; i++) {
+      lineNumbers += '<span class="line-number">'+i+'</span>\n';
+    }
+    var table = tableStart + lineNumbers + tableMiddle + '<pre>'+$('pre', code).html()+'</pre>' + tableEnd;
+    $(code).html(table);
+  });
+}
+
+disqus_init = false;
+
+function toggleDisqus() {
+  var dom = document.getElementById('disqus_thread');
+  if (dom.className.indexOf('hide') != -1) {
+    dom.className = dom.className.replace(/\bhide\b/,'');
+    if (!disqus_init) {
+        loadDisqus();
+        disqus_init = true;
+        // hacking for disqus bad height
+    }
+  } else {
+    dom.className += ' hide';
+  }
+  return false;
+}
+
+$('document').ready(function() {
+  testFeatures();
+  addCodeLineNumbers();
+});
+
+// iOS scaling bug fix
+// Rewritten version
+// By @mathias, @cheeaun and @jdalton
+// Source url: https://gist.github.com/901295
+(function(doc) {
+  var addEvent = 'addEventListener',
+      type = 'gesturestart',
+      qsa = 'querySelectorAll',
+      scales = [1, 1],
+      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+  function fix() {
+    meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+    doc.removeEventListener(type, fix, true);
+  }
+  if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+    fix();
+    scales = [0.25, 1.6];
+    doc[addEvent](type, fix, true);
+  }
+}(document));
+
